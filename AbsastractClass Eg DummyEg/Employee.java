ed275c5b9cd0586ba5498ffc894640304fcb7ce2
Hrutik Sargar <106005765+HrutikSargar@users.noreply.github.com>
2024-02-10 00:03:19 +0530

    Delete AbsastractClass Eg DummyEg directory

--- AbsastractClass Eg DummyEg/Employee.java
+++
@@ -1,8 +0,0 @@
-package AbstractClass.DummyEg;
-
-public  class Employee extends Manager{
-    @Override
-    void meth2() {
-        System.out.println("Meth2");
-    }
-}
--- AbsastractClass Eg DummyEg/Manager.java
+++
@@ -1,9 +0,0 @@
-package AbstractClass.DummyEg;
-
-public abstract class Manager extends Ceo{
-    @Override
-    void meth1() {
-        System.out.println("Meth1 in Manager");
-    }
-
-}
